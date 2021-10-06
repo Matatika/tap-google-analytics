e5462b3fa4a2f259b05f3ff45272b86da7f5ba58
danielpdwalker <danielpdwalker@gmail.com>
2021-10-06 10:31:13 +0100

    Added test to check that using client_id and client_secret with a refresh_url uses the correct credentials in ga_client

--- tap_google_analytics/tests/test_core.py
+++ tap_google_analytics/tests/test_core.py
@@ -22,16 +22,6 @@ class TestCore(unittest.TestCase):
         return self.requestBuilder
 
     def _mock_initialize_http(self):
-        mock_analyticsreporting_response = readfile(datafile('analyticsreporting-servicedocument.json'))
-        mock_analytics_response = readfile(datafile('analytics-servicedocument.json'))
-        self.mock_http = MockHttp(
-            [
-                # GET https://www.googleapis.com/discovery/v1/apis/analyticsreporting/v4/rest
-                MockResponse(data=mock_analyticsreporting_response),
-                # GET https://www.googleapis.com/discovery/v1/apis/analytics/v3/rest
-                MockResponse(data=mock_analytics_response)
-            ]
-        )
         return self.mock_http
 
     @patch('tap_google_analytics.ga_client.GAClient.initialize_http')
@@ -56,9 +46,73 @@ class TestCore(unittest.TestCase):
             }
         )
 
+        mock_analyticsreporting_response = readfile(datafile('analyticsreporting-servicedocument.json'))
+        mock_analytics_response = readfile(datafile('analytics-servicedocument.json'))
+        self.mock_http = MockHttp(
+            [
+                # GET https://www.googleapis.com/discovery/v1/apis/analyticsreporting/v4/rest
+                MockResponse(data=mock_analyticsreporting_response),
+                # GET https://www.googleapis.com/discovery/v1/apis/analytics/v3/rest
+                MockResponse(data=mock_analytics_response)
+            ]
+        )
+
         # when discover default catalog with 'authorization.bearer_token'
         catalog = tap_google_analytics.discover(mock_config)
 
         # expect valid catalog to be discovered
         self.assertEqual(len(catalog['streams']), 10, "Total streams from default catalog")
 
+
+    @patch('tap_google_analytics.ga_client.GAClient.initialize_http')
+    @patch('tap_google_analytics.ga_client.GAClient.initialize_requestbuilder')
+    def test_client_id_and_client_secret_with_refresh_proxy_url_discover(self,stub_initialize_requestbuilder , stub_initialize_http):
+        """ Test basic discover sync with client_id and client_secret"""
+        # given all required config
+        mock_config = {
+            'view_id': '123456789',
+            'start_date': datetime.datetime.now().strftime("%Y-%m-%d"),
+            'oauth_credentials': {
+                'refresh_proxy_url': 'mock-url',
+                'access_token': 'mock-token',
+                'refresh_token': 'mock-refresh-token',
+                'client_id': '123456789',
+                'client_secret' : '123456789'
+            },
+        }
+
+        # given mock service documents
+        mock_analyticsreporting_response = readfile(datafile('analyticsreporting-servicedocument.json'))
+        mock_analytics_response = readfile(datafile('analytics-servicedocument.json'))
+        # given mock metadata successful response
+        stub_initialize_requestbuilder.side_effect = self._mock_initialize_requestbuilder
+        mock_metadata_response = readfile(datafile('analytics-metadata-columns-list.json'))
+        self.requestBuilder = RequestMockBuilder(
+            {"analytics.metadata.columns.list": 
+                (None, mock_metadata_response)
+            }
+        )
+        # given mock service documents
+        mock_analyticsreporting_response = readfile(datafile('analyticsreporting-servicedocument.json'))
+        mock_analytics_response = readfile(datafile('analytics-servicedocument.json'))
+        # given mock successful metadata response        
+        mock_metadata_response = readfile(datafile('analytics-metadata-columns-list.json'))
+        # given mock http responses
+        mock_http_metadata = MockHttp(
+            [
+                # GET https://www.googleapis.com/discovery/v1/apis/analyticsreporting/v4/rest
+                MockResponse(data=mock_analyticsreporting_response),
+                # GET https://www.googleapis.com/discovery/v1/apis/analytics/v3/rest
+                MockResponse(data=mock_analytics_response),
+                # GET https://analytics.googleapis.com/analytics/v3/metadata/ga/columns?alt=json
+                MockResponse(data=mock_metadata_response)
+            ]
+        )
+        self.mock_http = mock_http_metadata
+        stub_initialize_http.side_effect = self._mock_initialize_http
+
+        # when discover default catalog with 'oauth_credentials.refresh_proxy_url'
+        catalog = tap_google_analytics.discover(mock_config)
+
+        # expect valid catalog to be discovered
+        self.assertEqual(len(catalog['streams']), 10, "Total streams from default catalog")
